7de21094314447b5fda479f9c926d8e6f376fbdc
hhuang <rooster_huang@yahoo.ca>
2015-07-18 23:35:33 -0400

    fix bug in calculation of function g

--- g.m
+++ g.m
@@ -1,5 +1,5 @@
 % phi is the angle between camera pointing direction minus
 % the angle of the distanceAndAngle function 
 function [result] = g(phi)
-    result = 1/(25*sqrt(2*pi))*exp(-25/(2*25));
+    result = 1/(25*sqrt(2*pi))*exp(-phi^2/(2*25));
 end
