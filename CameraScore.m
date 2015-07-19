032e9a57e4233f089894c4b8714c105017786d32
Nirmal Benann <njbenann@gmail.com>
2015-07-18 23:21:22 -0400

    Adding CameraScore.m

---
+++ CameraScore.m
@@ -0,0 +1,18 @@
+function [result] = CameraScore(M, x, y, theta)
+    
+    [nrows, ncols] = size(M);
+    V = zeros(nrows, ncols);
+    for i = 1:nrows
+        for j = 1:ncols
+            if (i~=x && j~=y)
+                element = M(i,j);
+                [distance_camera, angle_camera] = distanceAndAngle(x, y, i, j);
+                f_result = f(distance_camera);
+                phi = getPhi(theta, angle_camera);
+                g_result = g(phi);
+                V(i,j) = g_result * f_result * element;
+            end
+        end    
+    end
+    result = sum(sum(V));
+end
