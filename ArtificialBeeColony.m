b27a81f63b5c89b9bad4dddca7d43e0445e0b099
hhuang <rooster_huang@yahoo.ca>
2015-08-02 11:09:32 -0400

    Improve heuristic of ABC at cost of performance.

--- ArtificialBeeColony.m
+++ ArtificialBeeColony.m
@@ -4,7 +4,7 @@ function [ cameralist, score ] = ArtificialBeeColony( M, numCameras, colony_size
         abandon_limit = 5;
         iterations = 100;
     elseif nargin == 2
-        colony_size = 100;
+        colony_size = 8;
         abandon_limit = 5;
         iterations = 100;
     end
@@ -38,41 +38,48 @@ function [ cameralist, score ] = ArtificialBeeColony( M, numCameras, colony_size
         for ii = 1 : colony_size
             food_source = squeeze(colony(ii,:,:));
             initial_best_score = best_scores(1,ii);
+            best_score = 0;
+            current_score = 0;
+            temp_best = food_source;
             for jj = 1 : numCameras
+                possible_neighbour = food_source;
                 % separate change vs combined change
                 if rand < p_separate
                     if rand < p_mutate
                         temp = rand;
                         if temp < 0.2
-                            food_source(jj,1) = randi(nrows);
+                            possible_neighbour(jj,1) = randi(nrows);
                         elseif temp > 0.8
-                            food_source(jj,2) = randi(ncols);
+                            possible_neighbour(jj,2) = randi(ncols);
                         else
-                            food_source(jj,3) = randi(72)*5-1;
+                            possible_neighbour(jj,3) = randi(72)*5-5;
                         end 
+                        [~,current_score] = CameraScoresWithCamList(M,possible_neighbour);
                     end
                 else
                     if rand < p_mutate
-                        food_source(jj,1) = randi(nrows);
-                        food_source(jj,2) = randi(ncols);
-                        food_source(jj,3) = randi(72)*5-1;
+                        possible_neighbour(jj,1) = randi(nrows);
+                        possible_neighbour(jj,2) = randi(ncols);
+                        possible_neighbour(jj,3) = randi(72)*5-5;
+                        [~,current_score] = CameraScoresWithCamList(M,possible_neighbour);
                     end
                 end
+                
+                if current_score > best_score
+                    %onlooker chooses nectar to target
+                    best_score = current_score;
+                    temp_best = possible_neighbour;
+                end
             end
-            %onlooker chooses nectar to target
-            onlooker = randi(numCameras);
-            food_source(onlooker,:) = [food_source(onlooker,1),food_source(onlooker,1),randi(72)*5-1];
-            
+            food_source = temp_best;
             colony(ii,:,:) = food_source;
-            [~,current_score] = CameraScoresWithCamList(M,food_source);
-            
-            if current_score > initial_best_score
-                best_scores(1,ii) = current_score;
+            if best_score > initial_best_score
+                best_scores(1,ii) = best_score;
                 abandon_values(1,ii) = 0;
-                p_mutate = p_mutate / 2;
+                p_mutate = p_mutate *0.8;
             else
                 abandon_values(1,ii) = abandon_values(1,ii)+1;
-                p_mutate = p_mutate * 2;
+                p_mutate = p_mutate * 1.25;
                 if abandon_values(1,ii) > abandon_limit
                     %employee becomes scout for new food source
                     for jj = 1 : numCameras
