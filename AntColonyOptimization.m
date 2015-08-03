2c2b6dba13f1932e21b1c25d2c479950782e8cd6
YeounJun Park <pyj4104@hotmail.com>
2015-08-03 14:26:38 -0400

    v4

--- AntColonyOptimization.m
+++ AntColonyOptimization.m
@@ -3,42 +3,33 @@ function [ResultingCamList, ScoreOfResultCamList, PheromoneDepositMatrixr] = Ant
     [nRow, nColumn] = size(M);
     TotalDegree = 360;
     VaryingDegree = 8;
+    EvaporationRate = 0.5;
     %Construct Pheromone Deposit
-    PheromoneDepositMatrix = ones(nRow, nColumn, VaryingDegree) * 1;
+    PheromoneDepositMatrix = ones(nRow, nColumn, VaryingDegree);
     %Construct Value Matrix
-    ValueMatrix = zeros(nRow, nColumn, VaryingDegree);
-    %for i=1:nRow
-        %for j=1:nColumn
-         %   for k=1:VaryingDegree
-          %      ValueMatrix(i,j,k) = MatrixSummation(CameraScore(M, i, j, (k-1)*TotalDegree/VaryingDegree));
-           % end
-    %    end
-    %end
+    ValueMatrix = ones(nRow, nColumn, VaryingDegree);
     %ACO
     for iteration=1:it
         %Construct Movement Record Matrix
-        MovementRecordMatrix = zeros(nRow, nColumn, VaryingDegree);
         DeltaValueMatrix = zeros(nRow, nColumn, VaryingDegree);
         fprintf('Iteration: %d\n', iteration);
         for ant=1:NumAnts
             CamList = zeros(NumCam, 3);
             for camera=1:NumCam
                 camPosition = CameraPlacement(PheromoneDepositMatrix, ValueMatrix);
-                MovementRecordMatrix(camPosition(1), camPosition(2), camPosition(3)) = ...
-                    MovementRecordMatrix(camPosition(1), camPosition(2), camPosition(3)) + ...
-                    ((10^floor(log10(NumAnts)))*10)/NumAnts;
                 CamList(camera,:) = camPosition;
             end
             CamListEval = CamList;
             CamListEval(:,3) = CamListEval(:,3)*TotalDegree/VaryingDegree;
-            [~, deltaCost] = CameraScoresWithCamList(M, CamListEval);
-            deltaCost = deltaCost*10;
+            [~, deltaValue] = CameraScoresWithCamList(M, CamListEval);
+            deltaValue = deltaValue*10;
             for camera=1:NumCam
-                DeltaValueMatrix(CamList(camera, 1), CamList(camera, 2), CamList(camera, 3)) = deltaCost/NumCam;
+                DeltaValueMatrix(CamList(camera, 1), CamList(camera, 2), CamList(camera, 3)) = ...
+                    DeltaValueMatrix(CamList(camera, 1), CamList(camera, 2), CamList(camera, 3)) + deltaValue/NumCam;
             end
         end
         ValueMatrix = ValueMatrix + DeltaValueMatrix;
-        PheromoneDepositMatrix = PheromoneDepositMatrix*0.8 + MovementRecordMatrix*deltaCost;
+        PheromoneDepositMatrix = PheromoneDepositMatrix*EvaporationRate + DeltaValueMatrix;
     end
     %Return Cam List
     ResultingCamList = zeros(NumCam, 3);
@@ -57,29 +48,19 @@ function summation = MatrixSummation(M)
 end
 
 function [CamPos] = CameraPlacement(PheromoneDepositMatrix, ValueMatrix)
-    TotalProb = MatrixSummation(PheromoneDepositMatrix) + MatrixSummation(ValueMatrix);
+    TotMat = (PheromoneDepositMatrix.*ValueMatrix);
+    TotalProb = MatrixSummation(TotMat)^2;
     pos = rand * TotalProb;
     [xMax, yMax, thetaMax] = size(ValueMatrix);
-    x = 1;
-    y = 1;
-    theta = 0;
-    while pos > 0
-        theta = theta + 1;
-        if theta > thetaMax
-            theta = 1;
-            y = y + 1;
+    for x=1:xMax
+        for y=1:yMax
+            for theta=1:thetaMax
+                CamPos = [x, y, theta];
+                pos = pos - (TotMat(x, y, theta))^0.5;
+                if pos < 0
+                    return;
+                end
+            end
         end
-        if y > yMax
-            y = 1;
-            x = x + 1;
-        end
-        if x > xMax
-            msgID = 'MYFUN:incorrectSize';
-            msg = 'x position is too large.';
-            baseException = MException(msgID,msg);
-            throw(baseException)
-        end
-        pos = pos - (PheromoneDepositMatrix(x, y, theta) + ValueMatrix(x, y, theta));
     end
-    CamPos = [x, y, theta];
 end
