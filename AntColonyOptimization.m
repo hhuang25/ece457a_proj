a80c2a2a6b013675e7707361479cc98806234300
YeounJun Park <pyj4104@hotmail.com>
2015-08-03 01:53:47 -0400

    V3. Now can run 50 by 50.

--- AntColonyOptimization.m
+++ AntColonyOptimization.m
@@ -4,39 +4,41 @@ function [ResultingCamList, ScoreOfResultCamList, PheromoneDepositMatrixr] = Ant
     TotalDegree = 360;
     VaryingDegree = 8;
     %Construct Pheromone Deposit
-    PheromoneDepositMatrix = ones(nRow, nColumn, VaryingDegree);
+    PheromoneDepositMatrix = ones(nRow, nColumn, VaryingDegree) * 1;
     %Construct Value Matrix
-    CostMatrix = zeros(nRow, nColumn, VaryingDegree);
-    for i=1:nRow
-        for j=1:nColumn
-            for k=1:VaryingDegree
-                CostMatrix(i,j,k) = MatrixSummation(CameraScore(M, i, j, (k-1)*TotalDegree/VaryingDegree));
-            end
-        end
-    end
+    ValueMatrix = zeros(nRow, nColumn, VaryingDegree);
+    %for i=1:nRow
+        %for j=1:nColumn
+         %   for k=1:VaryingDegree
+          %      ValueMatrix(i,j,k) = MatrixSummation(CameraScore(M, i, j, (k-1)*TotalDegree/VaryingDegree));
+           % end
+    %    end
+    %end
     %ACO
     for iteration=1:it
         %Construct Movement Record Matrix
         MovementRecordMatrix = zeros(nRow, nColumn, VaryingDegree);
-        %DeltaCostMatrix = zeros(nRow, nColumn, VaryingDegree);
+        DeltaValueMatrix = zeros(nRow, nColumn, VaryingDegree);
         fprintf('Iteration: %d\n', iteration);
         for ant=1:NumAnts
             CamList = zeros(NumCam, 3);
             for camera=1:NumCam
-                camPosition = CameraPlacement(PheromoneDepositMatrix, CostMatrix);
-                MovementRecordMatrix(camPosition(1), camPosition(2), camPosition(3)) = MovementRecordMatrix(camPosition(1), camPosition(2), camPosition(3)) + 1;
+                camPosition = CameraPlacement(PheromoneDepositMatrix, ValueMatrix);
+                MovementRecordMatrix(camPosition(1), camPosition(2), camPosition(3)) = ...
+                    MovementRecordMatrix(camPosition(1), camPosition(2), camPosition(3)) + ...
+                    ((10^floor(log10(NumAnts)))*10)/NumAnts;
                 CamList(camera,:) = camPosition;
             end
             CamListEval = CamList;
             CamListEval(:,3) = CamListEval(:,3)*TotalDegree/VaryingDegree;
             [~, deltaCost] = CameraScoresWithCamList(M, CamListEval);
             deltaCost = deltaCost*10;
-            %for camera=1:NumCam
-                %DeltaCostMatrix(CamList(camera, 1), CamList(camera, 2), CamList(camera, 3)) = deltaCost;
-            %end
+            for camera=1:NumCam
+                DeltaValueMatrix(CamList(camera, 1), CamList(camera, 2), CamList(camera, 3)) = deltaCost/NumCam;
+            end
         end
-        %CostMatrix = CostMatrix + DeltaCostMatrix*10;
-        PheromoneDepositMatrix = PheromoneDepositMatrix/5 + MovementRecordMatrix*deltaCost;
+        ValueMatrix = ValueMatrix + DeltaValueMatrix;
+        PheromoneDepositMatrix = PheromoneDepositMatrix*0.8 + MovementRecordMatrix*deltaCost;
     end
     %Return Cam List
     ResultingCamList = zeros(NumCam, 3);
@@ -54,10 +56,10 @@ function summation = MatrixSummation(M)
     summation = sum(M(:));
 end
 
-function [CamPos] = CameraPlacement(PheromoneDepositMatrix, CostMatrix)
-    TotalProb = MatrixSummation(PheromoneDepositMatrix) + MatrixSummation(CostMatrix);
+function [CamPos] = CameraPlacement(PheromoneDepositMatrix, ValueMatrix)
+    TotalProb = MatrixSummation(PheromoneDepositMatrix) + MatrixSummation(ValueMatrix);
     pos = rand * TotalProb;
-    [xMax, yMax, thetaMax] = size(CostMatrix);
+    [xMax, yMax, thetaMax] = size(ValueMatrix);
     x = 1;
     y = 1;
     theta = 0;
@@ -77,7 +79,7 @@ function [CamPos] = CameraPlacement(PheromoneDepositMatrix, CostMatrix)
             baseException = MException(msgID,msg);
             throw(baseException)
         end
-        pos = pos - (PheromoneDepositMatrix(x, y, theta) + CostMatrix(x, y, theta));
+        pos = pos - (PheromoneDepositMatrix(x, y, theta) + ValueMatrix(x, y, theta));
     end
     CamPos = [x, y, theta];
 end
